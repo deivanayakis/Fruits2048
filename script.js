ee5f5512a8de1808325cc0464112d57219e74d99
deivanayakis <deivanayaki20@gmail.com>
2024-01-21 13:05:05 +0530

    eigth commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.onload = function(){
 }
 
 function setGame(){
-    board = [[0,8,8,0],[0,0,16,0],[0,0,0,0],[0,0,0,0]]
+    board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
     for(let i=0;i<rows;i++)
     {
@@ -18,6 +18,49 @@ function setGame(){
             let num = board[i][j];
             updateTile(tile,num);
             document.getElementById("board").append(tile);
+        }
+    }
+    setTile();
+    setTile();
+}
+
+    function hasEmpty(){
+        for(let i=0;i<rows;i++)
+        {
+            for(let j=0;j<columns;j++)
+            {
+                if(board[i][j]==0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    function setTile(){
+
+        if(!hasEmpty()){
+            return;
+        }
+        let flag = false;
+        while(!flag)
+        {
+            let r=Math.floor(Math.random()*rows);
+            let c=Math.floor(Math.random()*columns);
+
+            if(board[r][c] == 0){
+                board[r][c] = 2;
+                let tile = document.getElementById(r.toString()+"-"+ c.toString());
+                let rn = Math.random();
+                var ch = rn < 0.5 ? 2 : 4;
+                if(ch==2){
+                    tile.classList.add("x2");
+                }
+                else{
+                    tile.classList.add("x4");
+                }
+                break;
+            }
         }
     }
 
@@ -35,6 +78,17 @@ function setGame(){
         if(e.code == "ArrowLeft"){
             slideLeft();
         }
+        else if(e.code == "ArrowRight"){
+            slideRight();
+        }
+        else if(e.code == "ArrowUp"){
+            slideUp();
+        }
+        else if(e.code == "ArrowDown"){
+            slideDown();
+        }
+        setTile();
+        document.getElementsByTagName("h2").innerText=score;
     })
 
 
@@ -78,5 +132,56 @@ function setGame(){
         }
     }
 
-    
-}
+    function slideRight()
+    {
+        for (let i=0;i<rows;i++)
+        {
+            let row = board[i];
+            row.reverse();
+            row = slide(row);
+            row.reverse();
+            board[i]=row;            
+
+            for(let j=0;j<columns;j++)
+            {
+                let tile = document.getElementById(i.toString()+"-"+j.toString());
+                let num = board[i][j];
+                updateTile(tile,num);
+            }
+        }
+    }
+
+    function slideUp()
+    {
+        for(let j=0;j<columns;j++)
+        {
+            let row = [board[0][j],board[1][j],board[2][j],board[3][j]];
+            row = slide(row);
+            for(let i=0;i<rows;i++)
+            {
+                board[i][j]=row[i];
+                let tile = document.getElementById(i.toString()+"-"+j.toString());
+                let num = board[i][j];
+                updateTile(tile,num);
+            }
+        }
+    }
+
+    function slideDown()
+    {
+        for(let j=0;j<columns;j++)
+        {
+            let row = [board[0][j],board[1][j],board[2][j],board[3][j]];
+            row.reverse();
+            row = slide(row);
+            row.reverse();
+            for(let i=0;i<rows;i++)
+            {
+                board[i][j]=row[i];
+                let tile = document.getElementById(i.toString()+"-"+j.toString());
+                let num = board[i][j];
+                updateTile(tile,num);
+            }
+        }
+    }
+
